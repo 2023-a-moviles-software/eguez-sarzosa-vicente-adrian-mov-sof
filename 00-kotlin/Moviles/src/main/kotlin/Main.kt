7caa9032cd075c97b04a89b9dc7d61b33772447f
Adrian Eguez <eadepto@hotmail.com>
2023-05-25 08:56:13 -0500

    hasta clase abstracta

--- 00-kotlin/Moviles/src/main/kotlin/Main.kt
+++ 00-kotlin/Moviles/src/main/kotlin/Main.kt
@@ -1,7 +1,99 @@
+import java.util.*
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // INMUTABLES (NO se reasignan "=")
+    val inmutable: String = "Adrian";
+    // inmutable = "Vicente";
+
+    // Mutables (Re asignar)
+    var mutable: String = "Vicente";
+    mutable = "Adrian";
+
+    //  val > var
+    // Duck Typing
+    var ejemploVariable = " Adrian Eguez "
+    val edadEjemplo: Int = 12
+    ejemploVariable.trim()
+    // ejemploVariable = edadEjemplo;
+
+    // Variable primitiva
+    val nombreProfesor: String = "Adrian Eguez"
+    val sueldo: Double = 1.2
+    val estadoCivil: Char = 'C'
+    val mayorEdad: Boolean = true
+    // Clases Java
+    val fechaNacimiento: Date = Date()
+
+    // SWITCH
+    val estadoCivilWhen = "C"
+    when (estadoCivilWhen){
+        ("C") -> {
+            println("Casado")
+        }
+        "S" -> {
+            println("Soltero")
+        }
+        else -> {
+            println("No sabemos")
+        }
+    }
+    val esSoltero = (estadoCivilWhen == "S")
+    val coqueteo = if (esSoltero) "Si" else "No"
+    calcularSueldo(10.00)
+    calcularSueldo(10.00, 12.00, 20.00)
+    calcularSueldo(10.00, bonoEspecial = 20.00) // Named Parameters
+    calcularSueldo(bonoEspecial = 20.00, sueldo = 10.00, tasa = 14.00) //  Parametros nombrados
+}
+
+abstract class NumerosJava{
+    protected val numeroUno: Int
+    private val numeroDos: Int
+    constructor(
+        uno: Int,
+        dos: Int
+    ){ // Bloque de codigo del constructor
+        this.numeroUno = uno
+        this.numeroDos = dos
+        println("Inicializando")
+    }
+}
+
+abstract class Numeros( // Constructor PRIMARIO
+    // Ejemplo:
+    // uno: Int, (Parametro (sin modificador de acceso))
+    // private var uno: Int, // Propiedad Publica Clase numeros.uno
+    // var uno: Int, // Propiedad de la clase (por defecto es PUBLIC)
+    // public var uno: Int,
+    protected val numeroUno: Int, // Propiedad de la clase protected numeros.numeroUno
+    protected val numeroDos: Int, // Propiedad de la clase protected numeros.numeroDos
+){
+    // var cedula: string = "" (public es por defecto)
+    // private valorCalculado: Int = 0 (private)
+    init { // bloque constructor primario
+        this.numeroUno; this.numeroDos; // this es opcional
+        numeroUno; numeroDos; // sin el "this", es lo mismo
+        println("Inicializando")
+    }
+}
+
+
+// void -> Unit
+fun imprimirNombre(nombre: String): Unit{
+    println("Nombre : ${nombre}") // template strings
+}
+fun calcularSueldo(
+    sueldo: Double, // Requerido
+    tasa: Double = 12.00, // Opcional (defecto)
+    bonoEspecial: Double? = null, // Opcion null -> nullable
+): Double{
+    // Int -> Int? (nullable)
+    // String -> String? (nullable)
+    // Date -> Date? (nullable)
+    if(bonoEspecial == null){
+        return sueldo * (100/tasa)
+    }else{
+        return sueldo * (100/tasa ) + bonoEspecial
+    }
 }
