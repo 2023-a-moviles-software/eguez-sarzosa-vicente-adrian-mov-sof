db68c89546ae4b231b1db0e3134c0fee1e83b29f
Adrian Eguez <eadepto@hotmail.com>
2023-06-29 07:59:37 -0500

    hasta intents

--- 01 - Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
+++ 01 - Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.movilessoftware2023a
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
 import android.widget.Button
 import androidx.activity.result.contract.ActivityResultContracts
@@ -20,6 +22,28 @@ class MainActivity : AppCompatActivity() {
                     // Logica Negocio
                     val data = result.data
                     "${data?.getStringExtra("nombreModificado")}"
+                }
+            }
+        }
+
+    val callbackIntentPickUri =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            result ->
+            if(result.resultCode === RESULT_OK){
+                if(result.data != null){
+                    if(result.data!!.data != null){
+                        val uri:Uri = result.data!!.data!!
+                        val cursor = contentResolver.query(uri, null, null, null,  null, null)
+                        cursor?.moveToFirst()
+                        val indiceTelefono = cursor?.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER
+                        )
+                        val telefono = cursor?.getString(indiceTelefono!!)
+                        cursor?.close()
+                        "Telefono ${telefono}"
+                    }
                 }
             }
         }
@@ -51,6 +75,21 @@ class MainActivity : AppCompatActivity() {
         botonListView.setOnClickListener {
             irActividad(BListView::class.java)
         }
+
+        val botonIntentImplicito = findViewById<Button>(R.id.btn_ir_intent_implicito)
+        botonIntentImplicito
+            .setOnClickListener {
+                val intentConRespuesta = Intent(
+                    Intent.ACTION_PICK,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                )
+                callbackIntentPickUri.launch(intentConRespuesta)
+            }
+        val botonIntentExplicito = findViewById<Button>(R.id.btn_ir_intent_explicito)
+        botonIntentExplicito
+            .setOnClickListener {
+                abrirActividadConParametros(CIntentExplicitoParametros::class.java)
+            }
     }
 
 
